78fd42b73253e31bfb7d159049edcae285a28ffa
Seantung <seantung@qq.com>
2022-04-10 18:10:58 +0800

    增加了新的代码 18点09

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+import math
+
 # 测试程序
 print("Hello")
 print("World")
@@ -7,4 +9,8 @@ print("*"*20)
 print(2**32)
 # 现在是17点55分
 
+
+print(math.cos(math.pi*0.33))
+print("math")
+# 现在是18点09分
 print()
